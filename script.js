c06bc0762c18633126eaf0442d2b943a0db5fa40
sahil verma <sahilvermajdp@gmail.com>
2024-07-26 17:35:11 +0530

    2nd commit

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const url = "https://newsapi.org/v2/everything?q=";
 window.addEventListener("load", () => fetchNews("Technology"));
 
 async function fetchNews(query) {
-    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
-    const data = await res.json();
-    bindData(data.articles);
+    try {
+        const response = await axios.get(`${url}${query}&apiKey=${API_KEY}`);
+        const data = response.data;
+        bindData(data.articles);
+    } catch (error) {
+        console.error("Error fetching news:", error);
+    }
 }
 
 function bindData(articles) {
